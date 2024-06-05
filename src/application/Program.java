c6dac4b98fda79be9c1cefe0819384ed836fcc37
Pedro Braga <pbrosa15@gmail.com>
2024-06-05 18:04:35 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entitties.Department;
+import model.entitties.Seller;
 
 public class Program {
     public static void main(String[] args) {
@@ -8,5 +11,7 @@ public class Program {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
         
+        Seller seller = new Seller(2, "Pedro", "pedro2gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
