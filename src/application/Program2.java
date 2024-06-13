881631889da6a4a387ef253414d0cdefd9fcc9d9
Pedro Braga <pbrosa15@gmail.com>
2024-06-13 17:49:24 -0300

    DepartmentDaoJDBC finished

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,17 +1,55 @@
 package application;
 
+import java.util.List;
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entitties.Department;
+
 
 public class Program2 {
     public static void main(String[] args) {
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        Scanner sc = new Scanner(System.in);
         
-        System.out.println("===== TEST 1 =====");
-        Department newDepartment = new Department(null, "João Bobo");
+        System.out.println("===== TEST 1 insert =====");
+        Department newDepartment = new Department(null, "Arts");
         departmentDao.insert(newDepartment);
         System.out.println(" Inserted new id: " + newDepartment.getId());
+
+        System.out.println();
+
+        System.out.println("===== TEST 2 findById =====");
+        Department department = departmentDao.findById(2);
+        System.out.println(department);
+
+        System.out.println();
+
+        System.out.println("===== TEST 3 update =====");
+        Department dep = departmentDao.findById(2);
+        dep.setName("ADM");
+        departmentDao.update(dep);
+        System.out.println("update completed");
+
+        System.out.println();
+
+        System.out.println("===== TEST 4 deleteById =====");
+        System.out.println("Enter id for delete");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Id deleted");
+
+        System.out.println();
+
+        System.out.println("===== TEST 5 findAll =====");
+		List<Department> list = departmentDao.findAll();
+		for (Department d : list) {
+			System.out.println(d);
+		}
+
+        sc.close();
+
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,10 +5,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-
+import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entitties.Department;
 
@@ -42,6 +44,7 @@ public class DepartmentDaoJDBC implements DepartmentDao{
                     int id = rs.getInt(1);
                     obj.setId(id);
                 }
+                DB.closeResultSet(rs);
             }
             else {
                 throw new DbException("Unexpected error! No rows affected!");
@@ -50,30 +53,118 @@ public class DepartmentDaoJDBC implements DepartmentDao{
        catch (SQLException e) {
         throw new DbException(e.getMessage());
        }
+       finally {
+        DB.closeStatement(st);
+       }
     }
 
     @Override
     public void update(Department obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+       PreparedStatement st = null;
+       try {
+
+        st = conn.prepareStatement(
+            "UPDATE department "
+            + "SET Name = ? "
+            + "WHERE Id = ?"
+        );
+
+        st.setString(1, obj.getName());
+        st.setInt(2, obj.getId());
+
+        st.executeUpdate();
+
+       }
+        catch (SQLException e) {
+        throw new DbException(e.getMessage());
+       }
+       finally {
+        DB.closeStatement(st);
+       }
     }
 
     @Override
     public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+       PreparedStatement st = null;
+
+       try {
+
+        st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+
+        st.setInt(1, id);
+        st.executeUpdate();
+
+       } 
+       catch (SQLException e) {
+        throw new DbIntegrityException(e.getMessage());
+       }
+       finally {
+        DB.closeStatement(st);
+       }
     }
 
     @Override
     public Department findById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            st = conn.prepareStatement(
+                "SELECT * FROM department WHERE Id = ? "
+            );
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                Department obj = new Department();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                return obj;
+            }
+
+            return null;
+
+        } 
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
     public List<Department> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+       PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+
+			st = conn.prepareStatement(
+				"SELECT * FROM department ORDER BY Name");
+			rs = st.executeQuery();
+
+			List<Department> list = new ArrayList<>();
+
+			while (rs.next()) {
+				Department obj = new Department();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				list.add(obj);
+			}
+			return list;
+            
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
     }
     
 }
